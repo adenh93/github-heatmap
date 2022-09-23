81c19561d56df7d39dc53b79a2bfbb010b3c93bb
Aden Herold <aden.herold1@gmail.com>
2022-09-23 16:25:05 +1000

    feat: added custom errors to src/errors.rs via thiserror

---
+++ src/errors.rs
@@ -0,0 +1,29 @@
+use thiserror::Error;
+use reqwest::StatusCode;
+
+#[derive(Error, Debug)]
+pub enum GithubError {
+    #[error("Unable to reach Github services. Try again later!")]
+    BadRequest(#[from] reqwest::Error),
+    
+    #[error("Unable to scrape Github profile: '{0}'")]
+    ScrapeFailure(StatusCode),
+
+    #[error("Unable to find Github profile at url: '{0}'")]
+    ProfileNotFound(String),
+}
+
+#[derive(Error, Debug)]
+pub enum HeatmapError {
+    #[error("Failed to query element '{alias}' with selector: '{selector}'")]
+    QueryElement { alias: String, selector: String },
+
+    #[error("Failed to query attribute '{attr}' on '{on_alias}'!")]
+    QueryAttribute { attr: String, on_alias: String },
+
+    #[error("Failed to parse attribute '{attr}' on '{on_alias}'!")]
+    ParseAttribute { attr: String, on_alias: String },
+
+    #[error("Failed to parse Heatmap nodes. Unknown node size scraped from Github frontend.")]
+    UnknownNodeFormat
+}
